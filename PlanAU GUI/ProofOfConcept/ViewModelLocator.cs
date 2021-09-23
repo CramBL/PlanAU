633d9cbeac354fea30dbfb1e628bc00417c4a510
miemouridsen <miemouridsen@hotmail.com>
2021-09-23 13:12:44 +0200

    Folders in proof of concept made

---
+++ PlanAU GUI/ProofOfConcept/ViewModelLocator.cs
@@ -0,0 +1,15 @@
+﻿using ProofOfConcept.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProofOfConcept
+{
+    class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get => new MainWindowViewModel();
+        }
+    }
+}
---
+++ PlanAU GUI/ProofOfConcept/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProofOfConcept.ViewModels
+{
+    class MainWindowViewModel
+    {
+        #region Properties
+        #endregion
+
+        #region Method
+        #endregion
+
+        #region Commands
+        #endregion
+
+
+        private DelegateCommand<string> select_Course;
+        public DelegateCommand<string> Select_Course =>
+            select_Course ?? (select_Course = new DelegateCommand<string>(ExecuteSelect_Course));
+
+        void ExecuteSelect_Course(string selectedCourse)
+        {
+            //Do something depending on the course
+        }
+    }
+}
