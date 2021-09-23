e18f34270839c578779f7e2bd637b71eefa4c249
miemouridsen <miemouridsen@hotmail.com>
2021-09-23 14:03:05 +0200

    Select commands added

--- PlanAU GUI/ProofOfConcept/ViewModels/MainWindowViewModel.cs
+++ PlanAU GUI/ProofOfConcept/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using ProofOfConcept.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -11,33 +12,49 @@ namespace ProofOfConcept.ViewModels
     {
         #region Properties
         private ObservableCollection<Models.ICourse> _selectedCourses;
-        public ObservableCollection<Models.ICourse> Courses
+        public ObservableCollection<Models.ICourse> SelectedCourses
         {
-            get => _courses;
-            set { SetProperty(ref _courses, value); }
+            get => _selectedCourses;
+            set { SetProperty(ref _selectedCourses, value); }
         }
-
         #endregion
 
         #region Method
         public MainWindowViewModel()
         {
-            Courses.Add(new Models.Course("PRJ"));
-            Courses.Add(new Models.Course("GUI"));
-            Courses.Add(new Models.Course("DAB"));
-            Courses.Add(new Models.Course(""));
-            Courses.Add(new Models.Course());
+            SelectedCourses.Add(new Course("PRJ"));
+            SelectedCourses.Add(new Course("GUI"));
+            SelectedCourses.Add(new Course("DAB"));
+            SelectedCourses.Add(new Course("SWT"));
+            SelectedCourses.Add(new Course("SWD"));
+            SelectedCourses.Add(new Course("NGK"));
         }
         #endregion
 
         #region Commands
-        private DelegateCommand<string> select_Course;
-        public DelegateCommand<string> Select_Course =>
-            select_Course ?? (select_Course = new DelegateCommand<string>(ExecuteSelect_Course));
+        private DelegateCommand<string> _selectOneCourse;
+        public DelegateCommand<string> Select_One_Course =>
+            _selectOneCourse ?? (_selectOneCourse = new DelegateCommand<string>(ExecuteSelectOneCourse));
 
-        void ExecuteSelect_Course(string selectedCourse)
+        void ExecuteSelectOneCourse(string selectedCourse)
         {
-            CurrentCourse = selectedCourse;
+            SelectedCourses.Clear();
+            SelectedCourses.Add(new Course(selectedCourse));
+        }
+
+        private DelegateCommand _selectAllCourses;
+        public DelegateCommand SelectAllCourses =>
+            _selectAllCourses ?? (_selectAllCourses = new DelegateCommand(ExecuteSelectAllCourses));
+
+        void ExecuteSelectAllCourses()
+        {
+            SelectedCourses.Clear();
+            SelectedCourses.Add(new Course("PRJ"));
+            SelectedCourses.Add(new Course("GUI"));
+            SelectedCourses.Add(new Course("DAB"));
+            SelectedCourses.Add(new Course("SWT"));
+            SelectedCourses.Add(new Course("SWD"));
+            SelectedCourses.Add(new Course("NGK"));
         }
         #endregion
     }
