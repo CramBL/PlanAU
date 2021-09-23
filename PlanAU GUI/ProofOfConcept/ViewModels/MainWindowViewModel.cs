533d5086859b01766ca8ef0e7f27caa9f2a47597
miemouridsen <miemouridsen@hotmail.com>
2021-09-23 13:48:08 +0200

    Working on viewmodel

--- PlanAU GUI/ProofOfConcept/ViewModels/MainWindowViewModel.cs
+++ PlanAU GUI/ProofOfConcept/ViewModels/MainWindowViewModel.cs
@@ -1,29 +1,44 @@
 ﻿using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace ProofOfConcept.ViewModels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : BindableBase
     {
         #region Properties
+        private ObservableCollection<Models.ICourse> _selectedCourses;
+        public ObservableCollection<Models.ICourse> Courses
+        {
+            get => _courses;
+            set { SetProperty(ref _courses, value); }
+        }
+
         #endregion
 
         #region Method
+        public MainWindowViewModel()
+        {
+            Courses.Add(new Models.Course("PRJ"));
+            Courses.Add(new Models.Course("GUI"));
+            Courses.Add(new Models.Course("DAB"));
+            Courses.Add(new Models.Course(""));
+            Courses.Add(new Models.Course());
+        }
         #endregion
 
         #region Commands
-        #endregion
-
-
         private DelegateCommand<string> select_Course;
         public DelegateCommand<string> Select_Course =>
             select_Course ?? (select_Course = new DelegateCommand<string>(ExecuteSelect_Course));
 
         void ExecuteSelect_Course(string selectedCourse)
         {
-            //Do something depending on the course
+            CurrentCourse = selectedCourse;
         }
+        #endregion
     }
 }
