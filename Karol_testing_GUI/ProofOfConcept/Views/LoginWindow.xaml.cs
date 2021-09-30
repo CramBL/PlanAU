fade98a8b906f2aeeb21aa2a93ac3984d3c3b748
Karol <karolperkowski.main@gmail.com>
2021-09-30 09:08:10 +0200

    connected Databasa with login GUI

--- Karol_testing_GUI/ProofOfConcept/Views/LoginWindow.xaml.cs
+++ Karol_testing_GUI/ProofOfConcept/Views/LoginWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
 
 namespace ProofOfConcept.Views
 {
@@ -43,19 +46,34 @@ namespace ProofOfConcept.Views
 
         private void TestButton_OnClick(object sender, RoutedEventArgs e)
         {
-            //SqlConnection con = new SqlConnection(@"Data Source = .\..."); -> ServerExplorer
-            //SqlAdapter sda = new SqlAdapter("SELECT Count(*) From Login where Username)'' + UserBox.Text + and Password = '' + passwordBox.Text + '', con")
-            //DataTable dt = new DataTable();
-            //sda.Fill(dt);
-            //if (dt.Row[0][0].ToString()=="1"){ -> to this what is to test
             this.Hide();
             MainWindow ss = new MainWindow();
             ss.Show();
 
-            //}
-            //else 
-            //{ MessageBox.Show("Please Check your Username or Password");
+           
         }
+
+        private void LoginButton_OnClickButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PlanAU;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); //if it is red need to be installed in nugets sqlclient
+            string query = "SELECT* from STUDENTS WHERE AU_ID = '" + userBox.Text.Trim()+ "'and PASSWORD = '"+ passwordBox.Text.Trim()+"'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            if(dtbl.Rows.Count == 1) //checking if there is a row for a user and his password
+            {
+                MainWindow ss = new MainWindow(); //if user name and password is correct then we create main window
+                this.Hide();                       //hide a login window
+                ss.Show();                          //and open main window
+            }
+            else
+            {
+                MessageBox.Show("Wrong Password or UserID, try again");
+            }
+
+
+        }
+       
     }
     
 
