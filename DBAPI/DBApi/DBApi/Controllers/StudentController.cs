3d137c6423e6659d2fc3d25da4856044c335f946
Rune Terp <runeterp@hotmail.com>
2021-10-04 20:58:15 +0200

    Added a .NET CORE 5 Web API for the DBAPI. Supports GET methods for all students or single student, and POST method for adding a student to database.

---
+++ DBAPI/DBApi/DBApi/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DBApi.Models;
+
+namespace DBApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private StudentContext studentContext;
+
+        public StudentController(StudentContext context)
+        {
+            studentContext = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return studentContext.STUDENTS;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent (Student student)
+        {
+            studentContext.STUDENTS.Add(student);
+            await studentContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStudent), new { id = student.AU_ID }, student);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(string id)
+        {
+            var todoItem = await studentContext.STUDENTS.FindAsync(id);
+
+            if (todoItem == null) { return NotFound(); }
+
+            return todoItem;
+        }
+    }
+}
