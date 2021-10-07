4cc4983ece14e63158a5eae9f0a624ec1c96e56f
MetteThorn <au640153@post.au.dk>
2021-10-07 09:52:29 +0200

    Oprettede desktop application som et prism project og kopierede PoC af GUI ind i

---
+++ Desktop Application/Desktop Application/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Desktop_Application.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace Desktop_Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
