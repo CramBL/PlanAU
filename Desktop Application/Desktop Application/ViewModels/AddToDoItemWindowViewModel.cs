aa8e78c4613828335b196c80241d825a50e6c720
Karol Perkowski <73069838+karolper@users.noreply.github.com>
2021-10-07 15:51:22 +0200

    ToDo ToDoWindowView

--- Desktop Application/Desktop Application/ViewModels/AddToDoItemWindowViewModel.cs
+++ Desktop Application/Desktop Application/ViewModels/AddToDoItemWindowViewModel.cs
@@ -3,10 +3,12 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Prism.Services.Dialogs;
+using Prism.Commands;
 
 namespace Desktop_Application.ViewModels
 {
-    class AddToDoItemWindowViewModel : BindableBase
+    class AddToDoItemWindowViewModel : BindableBase, IDialogAware
     {
         private ToDoItem _toDoItem;
         public ToDoItem TodDoItem
@@ -14,5 +16,54 @@ namespace Desktop_Application.ViewModels
             get { return _toDoItem; }
             set { SetProperty(ref _toDoItem, value); }
         }
+
+        public bool CanCloseDialog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDialogClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Title { get; }
+        public event Action<IDialogResult> RequestClose;
+
+
+
+        private DelegateCommand _cancelButton;
+        public DelegateCommand CancelButton =>
+            _cancelButton ?? (_cancelButton = new DelegateCommand(ExecuteCancelButton));
+
+        void ExecuteCancelButton()
+        {
+            //closing window
+        }
+
+        private DelegateCommand _okCommand;
+        public DelegateCommand OKCommand =>
+            _okCommand ?? (_okCommand = new DelegateCommand(ExecuteOKCommand));
+
+        void ExecuteOKCommand()
+        {
+            //adding new ToDo iteam
+
+        }
+
+
     }
+
+
+
+
+
+    
+
+
 }
