cc63bd19a60768ab2fb8d560b8b207d446944cd3
runeterp <runeterp@hotmail.com>
2021-10-07 13:37:20 +0200

    DAL finished

---
+++ DAL/PRJ4_DAL/PRJ4_DAL/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PRJ4_DAL
+{
+    public class Student
+    {
+        [JsonPropertyName("aU_ID")]
+        public string Au_id { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+    }
+}
