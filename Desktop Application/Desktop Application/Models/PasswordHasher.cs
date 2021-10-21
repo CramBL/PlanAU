327834172cdd60d858c1a30d11463123ebc5151b
CramBL <marcbkonig@gmail.com>
2021-10-21 21:38:05 +0200

    Create PasswordHasher class
    Function to generate random salted hash from password and return hash + salt. Other function to evaluate a password against a hash + salt.

---
+++ Desktop Application/Desktop Application/Models/PasswordHasher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Desktop_Application.Models
+{
+    //Adapted from https://www.cidean.com/blog/2019/password-hashing-using-rfc2898derivebytes/
+    public class PasswordHasher
+    {
+        /// Return a string delimited with random salt and hashed password
+        public string Generate(string password, int iterations = 10000)
+        {
+            //generate a random salt for hashing
+            var salt = new byte[24];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+
+            //hash password given salt and iterations
+            //iterations provide difficulty when cracking (10000 recommended)
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            byte[] hash = pbkdf2.GetBytes(24);
+            
+            //return delimited string with salt | hash
+            return Convert.ToBase64String(salt) + "|" +
+                Convert.ToBase64String(hash);
+            
+        }
+
+        /// Returns true of hash of test password matches hashed password within origDelimHash
+        public bool IsValid(string testPassword, string origDelimHash)
+        {
+            //extract original values from delimited hash text
+            var origHashedParts = origDelimHash.Split('|');
+            var origSalt = Convert.FromBase64String(origHashedParts[0]);
+            var origHash = origHashedParts[1];
+
+            //generate hash from test password and original salt and iterations
+            var pbkdf2 = new Rfc2898DeriveBytes(testPassword, origSalt, 10000);
+            byte[] testHash = pbkdf2.GetBytes(24);
+            
+            //if hash values match
+            if (Convert.ToBase64String(testHash) == origHash)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
