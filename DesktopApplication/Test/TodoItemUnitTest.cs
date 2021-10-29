2329626079f1959dbec4d4bfdd21ff595a253ee5
Oliver Hennings <89971830+OliverSchriverHennings@users.noreply.github.com>
2021-10-29 08:50:42 +0200

    datetime type updated for test

--- DesktopApplication/Test/TodoItemUnitTest.cs
+++ DesktopApplication/Test/TodoItemUnitTest.cs
@@ -8,29 +8,30 @@ namespace Test
 {
     class TodoItemUnitTest
     {
-
+         DateTime date = new DateTime(2021, 10, 29);
         [SetUp]
         public void Setup()
         {
             
         }
 
-        [TestCase("Unit test", "lave unit test", "23/10-2021")]
-        public void ToDoItem_CreateNewInstanceWith3Parameters_ObjectCreated(string title, string description, string date)
+        [TestCase("Unit test", "lave unit test")]
+        public void ToDoItem_CreateNewInstanceWith3Parameters_ObjectCreated(string title, string description)
         {
-            ToDoItem task_1 = new ToDoItem(title,description,date);
+            ToDoItem task_1 = new ToDoItem(title,description,new DateTime(2021,10,28));
             Assert.AreEqual(title, task_1.ToDoTitle);
             Assert.AreEqual(description, task_1.ToDoDescription);
             Assert.AreEqual(date, task_1.Date);
+
             // TodoItem der bliver lavet med contructoren med 3 parametre har altid status false på done
             Assert.AreEqual(false, task_1.Done);
         }
 
 
-        [TestCase("Unit test","lave unit test","28-10-2021")]
-        public void CToDoItem_CreateNewInstanceWith4Parameters_ObjectCreated(string title, string description, string date)
+        [TestCase("Unit test","lave unit test")]
+        public void CToDoItem_CreateNewInstanceWith4Parameters_ObjectCreated(string title, string description)
         {
-            ToDoItem task_1 = new ToDoItem(title, description, date, true);
+            ToDoItem task_1 = new ToDoItem(title, description, new DateTime(2021,10,29), true);
             Assert.AreEqual(title, task_1.ToDoTitle);
             Assert.AreEqual(description, task_1.ToDoDescription);
             Assert.AreEqual(date, task_1.Date);
