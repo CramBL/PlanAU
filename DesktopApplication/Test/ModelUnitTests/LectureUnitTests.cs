4b61cace74313536b619c83c18182f47482b4266
CramBL <marcbkonig@gmail.com>
2021-11-02 19:28:18 +0100

    removed JsonSerializer classes and unit tests
    Oliver sagde det var ok

--- DesktopApplication/Test/ModelUnitTests/LectureUnitTests.cs
+++ DesktopApplication/Test/ModelUnitTests/LectureUnitTests.cs
@@ -16,11 +16,11 @@ namespace Test
 
         [TestCase("1","lave unit tets")]
 
-        public void creatingInstanceOfLecture(string number, string discription)
+        public void creatingInstanceOfLecture(string number, string description)
         {
-            Lecture lecture = new Lecture(number,discription);
+            Lecture lecture = new Lecture(number,description);
             Assert.AreEqual(number, lecture.Number);
-            Assert.AreEqual(discription, lecture.PreparationDescription);
+            Assert.AreEqual(description, lecture.PreparationDescription);
             Assert.AreEqual(null, lecture.CourseName);
             Assert.AreEqual(null, lecture.DateString);
             
