fa6585072c51b3137bdf730acd59415d76149fe3
Oliver Hennings <89971830+OliverSchriverHennings@users.noreply.github.com>
2021-11-22 09:26:39 +0100

    error handling on password in DBAPI

--- DBAPI/DBApi/DBApi/Models/PasswordHasher.cs
+++ DBAPI/DBApi/DBApi/Models/PasswordHasher.cs
@@ -20,19 +20,26 @@ namespace DBApi.Models
         public bool IsValid(string testPassword, string origDelimHash)
         {
             //extract original values from delimited hash text
-            var origHashedParts = origDelimHash?.Split('|');
-            var origSalt = Convert.FromBase64String(origHashedParts[0]);
-            var origHash = origHashedParts[1];
-
-            //generate hash from test password and original salt and iterations
-            using var pbkdf2 = new Rfc2898DeriveBytes(testPassword, origSalt, 100000);
-            byte[] testHash = pbkdf2.GetBytes(24);
-
-            //if hash values match
-            if (Convert.ToBase64String(testHash) == origHash)
-                return true;
-            else
+            try
+            {
+                var origHashedParts = origDelimHash?.Split('|');
+                var origSalt = Convert.FromBase64String(origHashedParts[0]);
+                var origHash = origHashedParts[1];
+                //generate hash from test password and original salt and iterations
+                using var pbkdf2 = new Rfc2898DeriveBytes(testPassword, origSalt, 100000);
+                byte[] testHash = pbkdf2.GetBytes(24);
+                //if hash values match
+                if (Convert.ToBase64String(testHash) == origHash)
+                    return true;
+                else
+                    return false;
+            }
+            catch (FormatException e)
+            {
                 return false;
+            }
+
+        
         }
     }
 }
